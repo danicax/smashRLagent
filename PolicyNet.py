967315a1ef40e10c1e999d614d174d23b8aa6354
danica <xiongdanica@gmail.com>
2025-05-22 03:06:31 -0700

    pog

--- PolicyNet.py
+++ PolicyNet.py
@@ -1,17 +1,19 @@
-import torch.nn as nn
 import torch
+import torch.nn as nn
+from torch.distributions import Normal
 
 class PolicyNet(nn.Module):
-    def __init__(self, obs_dim, act_dim):
+    def __init__(self, obs_dim, act_dim=17):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(obs_dim, 64), nn.ReLU(),
-            nn.Linear(64, 64),      nn.ReLU(),
-            #nn.Linear(64, act_dim)
+            nn.Linear(64,      64), nn.ReLU(),
         )
-        self.mu      = nn.Linear(64, act_dim)
-        self.logstd  = nn.Parameter(torch.zeros(act_dim))
+        self.mu     = nn.Linear(64, act_dim)         # mean for each of 17 dims
+        self.logstd = nn.Parameter(torch.zeros(act_dim))  # log-std for each
 
     def forward(self, x):
-        h = self.net(x)
-        return self.mu(h), self.logstd
+        h      = self.net(x)
+        mu     = self.mu(h)               # [B,17]
+        logstd = self.logstd             # [17]
+        return mu, logstd
--- eval.py
+++ eval.py
@@ -61,11 +61,17 @@ def unpack_and_send(controller, action_tensor):
         melee.enums.Button.BUTTON_L, melee.enums.Button.BUTTON_R, melee.enums.Button.BUTTON_X,
         melee.enums.Button.BUTTON_Y, melee.enums.Button.BUTTON_Z #, melee.enums.Button.BUTTON_START
     ]
+
+    maxButVal = 0
+    maxBut = None;
     for i, b in enumerate(btns):
-        # if(b==melee.enums.Button.BUTTON_L or b==melee.enums.Button.BUTTON_R):
-        #     continue
-        if action_tensor[i].item() >0.5:
-            controller.press_button(b)
+        if(b==melee.enums.Button.BUTTON_L or b==melee.enums.Button.BUTTON_R):
+            continue
+        if action_tensor[i].item() >0.5 and action_tensor[i].item() > maxButVal:
+            maxButVal = action_tensor[i].item()
+            maxBut = b
+    if maxBut is not None:
+        controller.press_button(maxBut)
             # if(b == melee.enums.Button.BUTTON_A):
             #     print("A")
 
